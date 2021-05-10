d4bc052616617e24157303be441c4e6298c5494a
Jens Petersen <petersen@redhat.com>
2021-05-10 10:49:45 +0800

    move Spec outside to extra/

--- extra/Spec.hs
+++ extra/Spec.hs
@@ -3,7 +3,7 @@
 -- the Free Software Foundation, either version 2 of the License, or
 -- (at your option) any later version.
 
-module Data.RPM.Spec (
+module RPM.Spec (
   specRead,
   showSpec
   ) where
